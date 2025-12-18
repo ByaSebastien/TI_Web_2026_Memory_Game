219f9b4b36801bb7601664f7d835067d361a214d
Bya Sebastien <byaebastien@hotmail.com>
2025-12-18 15:43:28 +0100

    add sounds

--- script.js
+++ script.js
@@ -54,73 +54,128 @@ let selected = [];
 let count = 0;
 let previousTarget = null;
 let delay = 1200;
+let correctGuesses = 0;
 
-const game = document.getElementById('game');
+const intro = new Audio('./sounds/intro.mp3');
+const dmc = new Audio('./sounds/start.mp3');
 
-gameCards.forEach(item => {
-    const { name, img } = item;
+init();
 
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.dataset.name = name;
+function init() {
 
-    const front = document.createElement('div');
-    front.classList.add('front');
+    intro.play();
 
-    const back = document.createElement('div');
-    back.classList.add('back');
-    back.style.backgroundImage = `url(${img})`;
+    const tl = gsap.timeline();
 
-    game.appendChild(card);
-    card.appendChild(front);
-    card.appendChild(back);
-});
+    tl.from('.main-logo', {
+        opacity: 0,
+        clipPath: "inset(0 100% 100% 0)",
+        duration: 3,
+        ease: "power2.inOut"
+    }).from('.start-btn', {
+        opacity: 0,
+        clipPath: "inset(0 0 100% 0)",
+        duration: 1,
+        ease: "power2.inOut"
+    }, '+=1.8');
+}
 
-const match = () => {
-    selected.forEach(card => {
-        card.classList.add('match');
-    });
-};
+function startGame() {
 
-const resetGuesses = () => {
-    count = 0;
-    previousTarget = null;
-
-    selected.forEach(card => {
-        card.classList.remove('selected');
-    });
+    intro.pause();
+    dmc.play();
 
     selected = [];
-};
+    count = 0;
+    previousTarget = null;
+    correctGuesses = 0;
 
-game.addEventListener('click', event => {
+    document.body.innerText = '';
+    const game = document.createElement('div');
+    game.classList.add('game');
+    document.body.appendChild(game);
 
-    const clicked = event.target.parentNode;
+    gameCards.forEach(item => {
+        const { name, img } = item;
 
-    if (
-        clicked === previousTarget ||
-        clicked.classList.contains('selected') ||
-        clicked.classList.contains('match')
-    ) {
-        return;
-    }
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.name = name;
 
-    if (count < 2) {
-        count++;
-        if (count === 1) {
-            clicked.classList.add('selected');
-        } else {
-            clicked.classList.add('selected');
+        const front = document.createElement('div');
+        front.classList.add('front');
+
+        const back = document.createElement('div');
+        back.classList.add('back');
+        back.style.backgroundImage = `url(${img})`;
+
+        game.appendChild(card);
+        card.appendChild(front);
+        card.appendChild(back);
+    });
+
+    gsap.from(".card", {
+        opacity: 0,
+        duration: 1,
+        stagger: 0.1,
+    });
+
+
+    const match = () => {
+        selected.forEach(card => {
+            card.classList.add('match');
+        });
+        correctGuesses++;
+
+        setTimeout(checkVictory, delay);
+    };
+
+    const resetGuesses = () => {
+        count = 0;
+        previousTarget = null;
+
+        selected.forEach(card => {
+            card.classList.remove('selected');
+        });
+
+        selected = [];
+    };
+
+    game.addEventListener('click', event => {
+
+        const clicked = event.target.parentNode;
+
+        if (
+            clicked === previousTarget ||
+            clicked.classList.contains('selected') ||
+            clicked.classList.contains('match')
+        ) {
+            return;
         }
-        selected.push(clicked);
 
-        if (selected.length === 2) {
-            const [first, second] = selected.map(n => n.dataset.name);
-            if (first === second) {
-                setTimeout(match, delay);
+        if (count < 2) {
+            count++;
+            if (count === 1) {
+                clicked.classList.add('selected');
+            } else {
+                clicked.classList.add('selected');
             }
-            setTimeout(resetGuesses, delay);
+            selected.push(clicked);
+
+            if (selected.length === 2) {
+                const [first, second] = selected.map(n => n.dataset.name);
+                if (first === second) {
+                    setTimeout(match, delay);
+                }
+                setTimeout(resetGuesses, delay);
+            }
+            previousTarget = clicked;
         }
-        previousTarget = clicked;
+    });
+}
+
+function checkVictory() {
+    if (correctGuesses === 1) {
+        //TODO New Game?
     }
-});
+}
